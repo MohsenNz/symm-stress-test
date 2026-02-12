da18896faf0939792eab1df163dce363ec018efc
Mohsen <thelight.mn@gmail.com>
2026-02-12 16:11:26 +0330

    fix

--- 5-txSpammer-writeLoad.sh
+++ 5-txSpammer-writeLoad.sh
@@ -1,5 +1,5 @@
 for i in {1..10000}; do
-  cast send "$ADDR" "stress(uint256,uint256)" 1 0 \
+  cast send 0x356380855afCb805d4Fc1f55e92089a05BEADF18 "setNumber(uint256)" "$i" \
     --private-key "$PK" \
     --rpc-url "$RPC_URL"
 done
