48864afddbec98ad5a4d96b7268e4fdab0c1d1fa
Mohsen <thelight.mn@gmail.com>
2026-02-14 11:18:04 +0330

    fix

--- 5-txSpammer-writeLoad.sh
+++ 5-txSpammer-writeLoad.sh
@@ -1,6 +1,6 @@
 NONCE=$(cast nonce "$WALLET" --rpc-url "$RPC_URL")
 
-for i in {0..3}; do
+for i in {0..10000}; do
   cast send 0x356380855afCb805d4Fc1f55e92089a05BEADF18 "setNumber(uint256)" "$i" \
     --nonce $((NONCE+i)) \
     --gas-limit 100000 \
